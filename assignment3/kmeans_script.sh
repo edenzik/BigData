0fd593cadf33bb824f90e171f43669f808a54d4a
Kahlil Oppenheimer <kahliloppenheimer@gmail.com>
2014-11-02 16:08:24 -0500

    Added fuzzy k-means to Eden's script and fixed clusterdump input argument

--- assignment3/kmeans_script.sh
+++ assignment3/kmeans_script.sh
@@ -4,10 +4,19 @@
 #Arguments:
 # $1 = Convergnce Delta (-cd) (used to be 0.05)
 # $2 = Number of iterations (used to be 10)
-# $3 = Test number (the file location will be saved as eden_clustering_output/kmeans_run_$1)
-# $4 = ngram number (can be 1,2,3, or 4)
+# $3 = ngram number (can be 1,2,3, or 4)
+# $4 = Test number (the file location will be saved as eden_clustering_output/kmeans_run_$1)
 
-echo "EXECUTING CLUSTERING WITH PARAMETERS: Convergence Delta - $1 Number of Iterations - $2"
-mahout/bin/mahout kmeans -i filtered_tfidf/l2_$4gram/tfidf-vectors/ -c clusters_1 -o eden_clustering_output/kmeans_run_$3_$4_ngrams -cd $1 -x $2 -k 10 -cl -ow
+# Run Normal kmeans
+echo "EXECUTING K-MEANS CLUSTERING WITH PARAMETERS: Convergence Delta - $1 Number of Iterations - $2"
+mahout/bin/mahout kmeans -i filtered_tfidf/l2_$3gram/tfidf-vectors/ -c clusters_1 -o eden_clustering_output/kmeans_run_$4_$3_ngrams -cd $1 -x $2 -k 10 -cl -ow
 
-mahout/bin/mahout clusterdump -d filtered_tfidf/l2_$4gram/dictionary.file-0 -dt sequencefile -i eden_clustering_output/kmeans_run_$3/clusters-2-final -n 10 -b 100 -o clusterdump_output/k_means_run_$3_$4_ngrams_clusterdump_output
+# Run Fuzzy kmeans
+echo "EXECUTING FUZZY K-MEANS CLUSTERING WITH PARAMETERS: Convergence Delta - $1 Number of Iterations - $2"
+mahout/bin/mahout fkmeans -i filtered_tfidf/l2_$3gram/tfidf-vectors/ -c clusters_1 -o eden_clustering_output/fkmeans_run_$4_$3_ngrams -cd $1 -x $2 -k 10 -cl -ow	
+
+# Dump normal kmeans output
+mahout/bin/mahout clusterdump -d filtered_tfidf/l2_$3gram/dictionary.file-0 -dt sequencefile -i eden_clustering_output/kmeans_run_$4_$3_ngrams/clusters-2-final -n 10 -b 100 -o clusterdump_output/kmeans_run_$4_$3_ngrams_clusterdump_output
+
+# Dump Fuzzy means output
+mahout/bin/mahout clusterdump -d filtered_tfidf/l2_$3gram/dictionary.file-0 -dt sequencefile -i eden_clustering_output/fkmeans_run_$4_$3_ngrams/clusters-2-final -n 10 -b 100 -o clusterdump_output/fkmeans_run_$4_$3_ngrams_clusterdump_output
